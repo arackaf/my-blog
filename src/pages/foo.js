2630f283088579d5376ecdbd01c78059aefe1473
Adam.Rackis <adam.rackis@riotgames.com>
2019-02-27 16:03:09 -0600

    initial checkin

---
+++ src/pages/foo.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+export default () => {
+  return (
+    <div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1>Hello World</h1>
+    </div>
+  )
+}
