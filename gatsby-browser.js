537e843383758fea5a5ec582b0fc627cd28cf75e
Adam Rackis <adamrackis@gmail.com>
2019-02-27 16:03:09 -0600

    initial checkin

---
+++ gatsby-browser.js
@@ -0,0 +1,8 @@
+// custom typefaces
+import "typeface-montserrat"
+import "typeface-merriweather"
+
+require("./main.css")
+require("prismjs/themes/prism-okaidia.css")
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+require("prismjs/plugins/line-highlight/prism-line-highlight.css")
