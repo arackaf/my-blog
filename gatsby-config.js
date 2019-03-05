21196d9d5efceb04787eb45905211c5ce42c4a70
Adam Rackis <adamrackis@gmail.com>
2019-03-04 22:39:19 -0600

    New icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -107,7 +107,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/icon3.png`,
       },
     },
     `gatsby-plugin-offline`,
