2ea1c18c1dcab51c56d4c3e78eea3af1678d396c
Adam.Rackis <adam.rackis@riotgames.com>
2019-03-04 23:38:16 -0600

    Misc metadata tweaks

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Adam Reacts`,
     subtitle: "(badly)",
     author: `Adam Rackis`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `A blog by Adam Rackis`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
       twitter: `kylemathews`,
