102c37e8b5854fdad78c0eae9b58c9eb114c390a
Adam Rackis <adamrackis@hotmail.com>
2022-02-06 16:55:26 -0600

    remove gatsby

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-// custom typefaces
-
-require("./main.css")
-require("./fontawesome/css/all.min.css")
-require("prismjs/themes/prism-okaidia.css")
-require("prismjs/plugins/line-numbers/prism-line-numbers.css")
-require("prismjs/plugins/line-highlight/prism-line-highlight.css")
