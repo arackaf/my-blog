2969b55ee5f9f2ac4a6602a83b4211103dae1e36
Adam Rackis <adamrackis@hotmail.com>
2022-02-06 20:10:23 -0600

    Removing un-needed things

--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { HOME_OG_IMAGE_URL } from "../lib/constants";
 
 export default function Meta() {
   return (
@@ -13,9 +12,7 @@ export default function Meta() {
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta name="description" content={`Adam Racki's personal site and blog.`} />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
   );
 }
