020faea5532eae4d50e9d9277169d5f1191ea156
Adam Rackis <adamrackis@hotmail.com>
2022-03-04 20:56:29 -0600

    Add ResizeObserver

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -3,10 +3,28 @@ import { useEffect, useRef } from "react";
 export default function PostBody({ content }) {
   const rootRef = useRef<HTMLDivElement>(null);
 
-  function highlightCode(pre, highlightRanges, lineNumberRowsContainer) {}
+  function highlightCode(pre, highlightRanges, lineNumberRowsContainer) {
+    const ranges = highlightRanges.split(",").filter(val => val);
+    const preWidth = pre.scrollWidth;
+
+    for (const range of ranges) {
+      let [start, end] = range.split("-");
+      if (!start || !end) {
+        start = range;
+        end = range;
+      }
+
+      for (let i = +start; i <= +end; i++) {
+        const lineNumberSpan: HTMLSpanElement = lineNumberRowsContainer.querySelector(`span:nth-child(${i})`);
+        lineNumberSpan.style.setProperty("--highlight-background", "rgba(100, 100, 100, 0.5)");
+        lineNumberSpan.style.setProperty("--highlight-width", `${preWidth}px`);
+      }
+    }
+  }
 
   useEffect(() => {
     const allPres = rootRef.current.querySelectorAll("pre");
+    const cleanup: (() => void)[] = [];
 
     for (const pre of allPres) {
       const code = pre.firstElementChild;
@@ -15,40 +33,27 @@ export default function PostBody({ content }) {
       }
 
       const highlightRanges = pre.dataset.line;
-      const lineNumberRowsContainer = pre.querySelector(".line-numbers-rows");
+      const lineNumbersContainer = pre.querySelector(".line-numbers-rows");
 
-      if (!highlightRanges || !lineNumberRowsContainer) {
+      if (!highlightRanges || !lineNumbersContainer) {
         continue;
       }
-      const ranges = highlightRanges.split(",").filter(val => val);
-      const preWidth = pre.scrollWidth;
 
-      for (const range of ranges) {
-        let [start, end] = range.split("-");
-        if (!start || !end) {
-          start = range;
-          end = range;
-        }
+      const runHighlight = () => highlightCode(pre, highlightRanges, lineNumbersContainer);
+      runHighlight();
 
-        for (let i = +start; i <= +end; i++) {
-          const lineNumberSpan: HTMLSpanElement =
-            lineNumberRowsContainer.querySelector(`span:nth-child(${i})`);
-          lineNumberSpan.style.setProperty(
-            "--highlight-background",
-            "rgba(100, 100, 100, 0.5)"
-          );
-          lineNumberSpan.style.setProperty(
-            "--highlight-width",
-            `${preWidth}px`
-          );
-        }
-      }
+      const ro = new ResizeObserver(runHighlight);
+      ro.observe(pre);
+
+      cleanup.push(() => ro.disconnect());
     }
+
+    return () => cleanup.forEach(f => f());
   }, []);
 
   return (
     <div ref={rootRef} className="max-w-2xl mx-auto">
-      <div style={{ display: "flex" }}>
+      <div style={{ width: "50%" }}>
         <div className={""} dangerouslySetInnerHTML={{ __html: content }} />
       </div>
     </div>
