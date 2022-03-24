4175a61d5df45275ea0461c85878388ba6a9d340
Adam Rackis <adamrackis@hotmail.com>
2022-03-23 17:25:32 -0700

    remove un-needed css import

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import "prismjs/themes/prism-tomorrow.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
-import "prismjs/plugins/line-highlight/prism-line-highlight.css";
 import "../styles/prism-overrides.css";
 import "../styles/reset.css";
 import "../styles/index.css";
