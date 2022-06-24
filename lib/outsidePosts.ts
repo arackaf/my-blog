48af2ca71a32b0c301acf7a25aa4bb95a9787da6
Adam Rackis <adamrackis@hotmail.com>
2022-06-24 07:03:00 -0500

    Add recent css-tricks posts

--- lib/outsidePosts.ts
+++ lib/outsidePosts.ts
@@ -1,21 +1,77 @@
 export const externalPosts = [
   {
-    title: "Making your web app work offline",
-    date: "2017-12-07T10:00:00.000Z",
-    description: "A gentle introduction to offline web development",
-    url: "https://css-tricks.com/making-your-web-app-work-offline-part-1/",
+    title: "Building Interoperable Web Components That Even Work With React",
+    date: "2022-06-07T10:00:00.000Z",
+    description: "An introduction to building web components, and then using them with JavaScript frameworks like Svelte and React",
+    url: "https://css-tricks.com/building-interoperable-web-components-react/",
+  },
+  {
+    title: "Inline Image Previews with Sharp, BlurHash, and Lambda Functions",
+    date: "2022-05-19T10:00:00.000Z",
+    description: "Displaying image previews with Base64 encoding, and Blurhash",
+    url: "https://css-tricks.com/inline-image-previews-with-sharp-blurhash-and-lambda-functions/",
+  },
+  {
+    title: "Syntax Highlighting (and More!) With Prism on a Static Site",
+    date: "2022-05-04T10:00:00.000Z",
+    description: "A deep dive on integrating Prism with Next.js for code highlighting",
+    url: "https://css-tricks.com/syntax-highlighting-prism-on-a-next-js-site/",
+  },
+
+  {
+    title: "Setting Up CloudFront to Host Your Web App",
+    date: "2022-04-28T10:00:00.000Z",
+    description: "Hosting your entire web app with AWS Cloudfront",
+    url: "https://css-tricks.com/setting-up-cloudfront-to-host-your-web-app/",
+  },
+  {
+    title: "Adding CDN Caching to a Vite Build",
+    date: "2022-04-04T10:00:00.000Z",
+    description: "Using AWS Cloudfront to serve static assets",
+    url: "https://css-tricks.com/adding-cdn-caching-to-a-vite-build/",
+  },
+  {
+    title: "Subsetting Font Awesome to Improve Performance",
+    date: "2022-02-17T10:00:00.000Z",
+    description: "How to pick only the Font Awesome icons you want for smaller bundles",
+    url: "https://css-tricks.com/subsetting-font-awesome-to-improve-performance/",
+  },
+
+  {
+    title: "Demystifying TypeScript Discriminated Unions",
+    date: "2022-01-27T10:00:00.000Z",
+    description: "Explaining TypeScript unions, and discriminated unions",
+    url: "https://css-tricks.com/typescript-discriminated-unions/",
+  },
+  {
+    title: "Making a Site Work Offline Using the VitePWA Plugin",
+    date: "2022-01-18T10:00:00.000Z",
+    description: "Diving deeper into Vite, and using the VitePWA plugin to improve your site's performance",
+    url: "https://css-tricks.com/vitepwa-plugin-offline-service-worker/",
+  },
+  {
+    title: "Adding Vite to Your Existing Web App",
+    date: "2022-01-11T10:00:00.000Z",
+    description: "A fun, painless introduction to Vite",
+    url: "https://css-tricks.com/adding-vite-to-your-existing-web-app/",
+  },
+  {
+    title: "React Suspense: Lessons Learned While Loading Data",
+    date: "2021-11-09T10:00:00.000Z",
+    description: "A deeper dive into data loading with Suspense",
+    url: "https://css-tricks.com/react-suspense-lessons-learned-while-loading-data/",
   },
   {
     title: "Svelte for the Experienced React Dev",
     date: "2021-05-21T10:00:00.000Z",
     description: "A high-level introduction to Svelte, from the perspective of an experienced React developer",
-    url: "https://css-tricks.com/svelte-and-spring-animations/",
+    url: "https://css-tricks.com/svelte-for-the-experienced-react-dev/",
   },
   {
     title: "Coordinating Svelte Animations With XState",
     date: "2021-04-07T10:00:00.000Z",
     description: "An introduction to XState, for simplifying Svelte animation code",
-    url: "https://css-tricks.com/svelte-and-spring-animations/",
+    url: "https://css-tricks.com/coordinating-svelte-animations-with-xstate/",
   },
   {
     title: "Svelte and Spring Animations",
@@ -23,6 +79,7 @@ export const externalPosts = [
     description: "A deep dive into Svelte's spring animation features",
     url: "https://css-tricks.com/svelte-and-spring-animations/",
   },
+
   {
     title: "Integrating TypeScript with Svelte",
     date: "2020-12-28T10:00:00.000Z",
@@ -60,27 +117,9 @@ export const externalPosts = [
     url: "https://css-tricks.com/react-suspense-in-practice/",
   },
   {
-    title: "Demystifying TypeScript Discriminated Unions",
-    date: "2022-01-27T10:00:00.000Z",
-    description: "Explaining TypeScript unions, and discriminated unions",
-    url: "https://css-tricks.com/typescript-discriminated-unions/",
-  },
-  {
-    title: "Making a Site Work Offline Using the VitePWA Plugin",
-    date: "2022-01-18T10:00:00.000Z",
-    description: "Diving deeper into Vite, and using the VitePWA plugin to improve your site's performance",
-    url: "https://css-tricks.com/vitepwa-plugin-offline-service-worker/",
-  },
-  {
-    title: "Adding Vite to Your Existing Web App",
-    date: "2022-01-11T10:00:00.000Z",
-    description: "A fun, painless introduction to Vite",
-    url: "https://css-tricks.com/adding-vite-to-your-existing-web-app/",
-  },
-  {
-    title: "React Suspense: Lessons Learned While Loading Data",
-    date: "2021-11-09T10:00:00.000Z",
-    description: "A deeper dive into data loading with Suspense",
-    url: "https://css-tricks.com/react-suspense-lessons-learned-while-loading-data/",
+    title: "Making your web app work offline",
+    date: "2017-12-07T10:00:00.000Z",
+    description: "A gentle introduction to offline web development",
+    url: "https://css-tricks.com/making-your-web-app-work-offline-part-1/",
   },
 ];
