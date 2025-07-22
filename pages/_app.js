94d092c92536f8a45b54be2204b7405044fab926
Adam Rackis <adamrackis@hotmail.com>
2025-07-21 22:34:59 -0500

    work

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import "../styles/index.css";
 import "../styles/app.scss";
 import "../styles/header.scss";
 import "../styles/fontawesome/css/all.min.css";
+import "../styles/shiki-overrides.css";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
