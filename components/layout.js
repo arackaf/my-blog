b4b594c2cbc55a46559735cc6cfcf9644bce32e3
Adam Rackis <adamrackis@hotmail.com>
2025-09-14 09:29:41 -0500

    work

--- components/layout.js
+++ components/layout.js
@@ -5,12 +5,10 @@ export default function Layout({ children, className = "" }) {
   return (
     <>
       <Meta />
-      <main className="flex flex-col max-w-2xl mx-auto px-4 sm:px-0">
+      <main className="flex flex-col max-w-[704px] mx-auto px-4">
         <Nav />
         <article className={className}>
-          {/* <div style={{ backgroundColor: "blue" }}></div> */}
           <section>{children}</section>
-          {/* <div style={{ backgroundColor: "red" }}></div> */}
         </article>
       </main>
     </>
