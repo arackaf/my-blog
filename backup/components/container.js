bac625bb0a0dd430eaca0cb4de1a724e00c94f0c
Adam Rackis <adamrackis@hotmail.com>
2025-10-20 13:02:23 -0500

    remove old backup

--- backup/components/container.js
+++
@@ -1,3 +0,0 @@
-export default function Container({ children }) {
-  return <div className="mt-4 sm:mt-10">{children}</div>;
-}
--- backup/postcss.config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
-module.exports = config;
