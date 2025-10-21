3ea4bc0ea382be480ab7dc667f871f4ae3c790fe
Adam Rackis <adamrackis@hotmail.com>
2025-10-20 22:28:34 -0500

    work

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,6 @@ import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
 
-const allPaths = [
-  "/blog/swift-codable-any",
-  "/blog/dynamo-introduction",
-  "/blog/suspense-explained",
-  "/blog/redux-in-hooks",
-  "/blog/css-modules",
-  "/blog/graphql-caching-and-micro",
-  "/blog/state-and-use-reducer",
-  "/blog/offline-web-development",
-  "/blog/new-beginnings",
-];
-
 const config = defineConfig({
   plugins: [
     // this is the plugin that enables path aliases
@@ -27,7 +15,9 @@ const config = defineConfig({
     tanstackStart({
       prerender: {
         enabled: true,
-        filter: ({ path }) => path === "/", // || allPaths.includes(path),
+        filter: ({ path }) => {
+          return path === "/" || /\/blog\/.+/.test(path);
+        },
       },
     }),
     nitro({ config: { preset: "node-server" } }),
