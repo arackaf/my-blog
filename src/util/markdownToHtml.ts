a7d01b6b6e4f3772b5f2da58556f853dd71d8abb
Adam Rackis <adamrackis@hotmail.com>
2025-10-21 00:16:16 -0500

    tweak markdown init

--- src/util/markdownToHtml.ts
+++ src/util/markdownToHtml.ts
@@ -1,51 +1,39 @@
 import Shiki from "@shikijs/markdown-it";
 import MarkdownIt from "markdown-it";
 
-//const { promise, resolve } = Promise.withResolvers<MarkdownIt>();
+export default async function markdownToHtml(markdown: string) {
+  const md = MarkdownIt({
+    html: true,
+  });
 
-//let md: Promise<MarkdownIt> = promise;
-
-//async function getMarkdownIt() {
-// if (md) {
-//   return md;
-// }
-
-const md = MarkdownIt({
-  html: true,
-});
-
-md.use(
-  await Shiki({
-    themes: {
-      light: "dark-plus",
-      dark: "dark-plus",
-    },
-    transformers: [
-      {
-        name: "line-numbers-pre",
-        preprocess: (_: string, options: any) => {
-          if (options?.meta?.__raw?.includes("lineNumbers")) {
-            options.attributes = {};
-            options.attributes.lineNumbers = true;
-          }
-        },
+  md.use(
+    await Shiki({
+      themes: {
+        light: "dark-plus",
+        dark: "dark-plus",
       },
-      {
-        name: "line-numbers-post",
-        postprocess: (html, options: any) => {
-          if (options?.attributes?.lineNumbers) {
-            return html.replace(/<pre /g, "<pre data-linenumbers ");
-          }
-          return html;
+      transformers: [
+        {
+          name: "line-numbers-pre",
+          preprocess: (_: string, options: any) => {
+            if (options?.meta?.__raw?.includes("lineNumbers")) {
+              options.attributes = {};
+              options.attributes.lineNumbers = true;
+            }
+          },
         },
-      },
-    ],
-  }),
-);
+        {
+          name: "line-numbers-post",
+          postprocess: (html, options: any) => {
+            if (options?.attributes?.lineNumbers) {
+              return html.replace(/<pre /g, "<pre data-linenumbers ");
+            }
+            return html;
+          },
+        },
+      ],
+    }),
+  );
 
-//   return md;
-// }
-
-export default function markdownToHtml(markdown: string) {
   return md.render(markdown);
 }
