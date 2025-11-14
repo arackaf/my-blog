db847ea06b5d352029e862ecde54bc3e7e4780de
Adam Rackis <adamrackis@hotmail.com>
2025-11-14 05:53:45 -0500

    remove code for highlighting

--- src/components/post-body.tsx
+++ src/components/post-body.tsx
@@ -3,25 +3,6 @@ import { useEffect, useRef } from "react";
 export default function PostBody({ content }: { content: string }) {
   const rootRef = useRef<HTMLDivElement>(null);
 
-  function highlightCode(pre: HTMLElement, highlightRanges: string, lineNumberRowsContainer: HTMLElement) {
-    const ranges = highlightRanges.split(",").filter(val => val);
-    const preWidth = pre.scrollWidth;
-
-    for (const range of ranges) {
-      let [start, end] = range.split("-");
-      if (!start || !end) {
-        start = range;
-        end = range;
-      }
-
-      for (let i = +start; i <= +end; i++) {
-        const lineNumberSpan: HTMLSpanElement = lineNumberRowsContainer.querySelector(`span:nth-child(${i})`)!;
-        lineNumberSpan.style.setProperty("--highlight-background", "rgba(100, 100, 100, 0.5)");
-        lineNumberSpan.style.setProperty("--highlight-width", `${preWidth}px`);
-      }
-    }
-  }
-
   function createCopyButton(codeEl: HTMLElement) {
     const button = document.createElement("button");
     button.classList.add("prism-copy-button");
@@ -54,21 +35,6 @@ export default function PostBody({ content }: { content: string }) {
       }
 
       pre.appendChild(createCopyButton(code as HTMLElement));
-
-      const highlightRanges = pre.dataset.line;
-      const lineNumbersContainer = pre.querySelector(".line-numbers-rows") as HTMLElement;
-
-      if (!highlightRanges || !lineNumbersContainer) {
-        continue;
-      }
-
-      const runHighlight = () => highlightCode(pre, highlightRanges, lineNumbersContainer);
-      runHighlight();
-
-      const ro = new ResizeObserver(runHighlight);
-      ro.observe(pre);
-
-      cleanup.push(() => ro.disconnect());
     }
 
     return () => cleanup.forEach(f => f());
