c75085ca34c54a1f7f0caa7228ad9ff1fb458a3c
Adam Rackis <adamrackis@hotmail.com>
2026-05-22 10:20:14 -0500

    remove Nitro

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { nitro } from "nitro/vite";
 
 const config = defineConfig({
   plugins: [
@@ -22,7 +21,6 @@ const config = defineConfig({
         concurrency: 1,
       },
     }),
-    nitro({ config: { preset: "node-server" } }),
     viteReact(),
   ],
 });
