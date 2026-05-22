7418e07dbebdd9159689c97d2b47cd15475017c7
Adam Rackis <adamrackis@hotmail.com>
2026-05-22 10:37:55 -0500

    strip more

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,38 +1,10 @@
 import { HeadContent, Scripts, createRootRoute } from "@tanstack/react-router";
 
-import globalStyles from "../styles/global.css?url";
-import codeHighlightingStyles from "../styles/code-highlighting-overrides.css?url";
-import blogPostStyles from "../styles/blog-styles.css?url";
 import Meta from "@/components/Meta";
 
 export const Route = createRootRoute({
   head: () => ({
-    meta: [
-      {
-        charSet: "utf-8",
-      },
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1",
-      },
-      {
-        title: "TanStack Start Starter",
-      },
-    ],
-    links: [
-      {
-        rel: "stylesheet",
-        href: globalStyles,
-      },
-      {
-        rel: "stylesheet",
-        href: codeHighlightingStyles,
-      },
-      {
-        rel: "stylesheet",
-        href: blogPostStyles,
-      },
-    ],
+    links: [],
   }),
 
   shellComponent: RootDocument,
