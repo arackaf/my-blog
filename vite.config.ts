efe511fd1c08cb0e5b971d77de5ffca942baba06
Adam Rackis <adamrackis@hotmail.com>
2026-05-24 09:53:25 -0400

    switch to Cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { nitro } from "nitro/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 const config = defineConfig({
   plugins: [
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ["./tsconfig.json"],
@@ -22,7 +23,6 @@ const config = defineConfig({
         concurrency: 1,
       },
     }),
-    nitro({ config: { preset: "node-server" } }),
     viteReact(),
   ],
 });
