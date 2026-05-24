a65e371b22aba36747918a409dcacfd887df8434
Adam Rackis <adamrackis@hotmail.com>
2026-05-24 10:13:03 -0400

    Revert "switch to Cloudflare"
    This reverts commit efe511fd1c08cb0e5b971d77de5ffca942baba06.
    
    # Conflicts:
    #	package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { cloudflare } from "@cloudflare/vite-plugin";
+import { nitro } from "nitro/vite";
 
 const config = defineConfig({
   plugins: [
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ["./tsconfig.json"],
@@ -18,6 +17,7 @@ const config = defineConfig({
         enabled: true,
       },
     }),
+    nitro({ config: { preset: "node-server" } }),
     viteReact(),
   ],
 });
